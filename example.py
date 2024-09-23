fcf40000da858cc60fcb120e451c56a639ce13f4
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2024-09-23 14:43:44 +0300

    modify file example.py from lesson 6

--- example.py
+++ example.py
@@ -1,8 +1,16 @@
 from flask import Flask
 
-# Это callable WSGI-приложение
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, Hexlet!'
+
+@app.get('/users')
+def users_get():
+    return 'GET /users'
+
+
+@app.post('/users')
+def users_post():
+    return 'POST /users'
