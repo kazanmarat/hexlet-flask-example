10847926dcc59eecab6ddc77beb5fa002e014283
Marat Mukhutdinov <kazanmarat2009@yandex.ru>
2024-09-24 14:50:05 +0300

    modify file from chapter 12

--- example.py
+++ example.py
@@ -1,14 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+users = [
+    {'id': 1, 'name': 'mike'},
+    {'id': 2, 'name': 'mishel'},
+    {'id': 3, 'name': 'adel'},
+    {'id': 4, 'name': 'keks'},
+    {'id': 5, 'name': 'kamila'}
+]
 
 @app.route('/')
 def hello_world():
     return 'Hello, Hexlet!'
 
-@app.get('/users')
+@app.get('/users/')
 def users_get():
-    return 'GET /users'
+    filtered_users = []
+    query = request.args.get('query', '')
+    for user in users:
+        if query in user['name']:
+            filtered_users.append(user)
+
+    return render_template(
+        'users/index.html',
+        users=filtered_users,
+        search=query,
+    )
 
 
 @app.post('/users')
